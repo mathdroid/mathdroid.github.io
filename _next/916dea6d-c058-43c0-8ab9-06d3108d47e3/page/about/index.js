b378bf3e0b6c49c039875575b8254a7e021d551b
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-11-14 12:31:35 +0700

    add next export output

---
+++ _next/916dea6d-c058-43c0-8ab9-06d3108d47e3/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([6],{275:function(e,t,u){e.exports=u(276)},276:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=u(14),l=n(o),r=u(7),a=n(r),f=u(8),d=n(f),i=u(15),c=n(i),s=u(16),p=n(s),_=u(4),h=n(_),v=function(e){function t(e){return(0,a.default)(this,t),(0,c.default)(this,(t.__proto__||(0,l.default)(t)).call(this,e))}return(0,p.default)(t,e),(0,d.default)(t,[{key:"componentDidMount",value:function(){console.log(this.props)}},{key:"render",value:function(){return h.default.createElement("div",null,"About whoa")}}]),t}(h.default.Component);t.default=v}},[275]);
+            return { page: comp.default }
+          })
+        
